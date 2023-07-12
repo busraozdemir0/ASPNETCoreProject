9387f4f9a9d2ed6b94176734f605c403619efbe9
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-07-12 12:48:06 +0300

    personelleri ve birimlerini listeleme

---
+++ ASPNETCoreProject/Controllers/PersonelController.cs
@@ -0,0 +1,17 @@
+﻿using ASPNETCoreProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ASPNETCoreProject.Controllers
+{
+    public class PersonelController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var personelList = context.Personels.Include(x=>x.Birim).ToList();
+            return View(personelList);
+        }
+    }
+}
